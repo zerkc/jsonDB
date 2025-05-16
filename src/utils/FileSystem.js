dda5c30b6a872208c9eaaa864216ecdd88cac988
Gustavo Gonzalez <gustavo.a.g.latorre@gmail.com>
2025-05-16 17:13:19 -0300

    for testing

--- src/utils/FileSystem.js
+++ src/utils/FileSystem.js
@@ -35,6 +35,7 @@ export class FileSystem {
         return line;
       }
     }*/
+    csonsole.log("x");
     return new Promise((done, reject) => {
       let buffer = Buffer.alloc(this.BUFFER_SIZE);
       fs.read(
@@ -47,10 +48,12 @@ export class FileSystem {
           if (err) {
             reject(err);
           }
+          csonsole.log(".");
           if (bytesRead > 0) {
             this.remaining += buffer.toString("utf8", 0, bytesRead);
           }
 
+          csonsole.log("-");
           // Procesar línea por línea
           let lines = this.remaining.split("\n");
           const line = lines.shift();
